66b996056c7a3afaea78222d8ea689daa92a679f
Riccardo Gallo <riccardogallo@MBP-di-Riccardo.homenet.telecomitalia.it>
2021-10-21 18:20:19 +0200

    first modifications

---
+++ MMIAB-Primer/monolith/classes/tests/__init__.py
@@ -0,0 +1,10 @@
+"""
+test for the funcitonalities
+
+write tests also for the functionalities in the skeleton
+
+make a request --> check that the response is the correct one (type of the response and the received txt)
+
+"""
+
+
