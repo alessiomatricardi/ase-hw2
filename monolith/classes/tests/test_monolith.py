889c7fe9c2a2344b8b63e74614bdd6d024a8bcc3
RiccardoBonini <riccardobonsi@hotmail.it>
2021-10-23 18:16:10 +0200

    Update test_monolith.py

--- monolith/classes/tests/test_monolith.py
+++ monolith/classes/tests/test_monolith.py
@@ -11,4 +11,31 @@ class TestApp(unittest.TestCase):
     def test_homepage(self):  
         app = tested_app.test_client()
         response = app.get("/", content_type='html/text')
+        
         self.assertEqual(response.status_code, 200)
+        
+        #self.assertEqual(response.data, b'Hi') #fails because the parameters have to be exactly the same
+
+        # response.data is the string of the HTML page
+        # assert b'Hi' in response.data
+        # assert b'Admin' in response.data
+
+    def test_login(self):  
+        app = tested_app.test_client()
+        data1 = { 'email' : 'example@example.com' , 'password' : 'admina' } # wrong password
+        response = app.post("/login", data = data1 , content_type='html/text')
+        
+        #self.assertEqual(response.status_code, 200)
+        
+        assert b'email' in response.data # returns to the login page, without the password inserted
+
+        app = tested_app.test_client()
+        data2 = { "email" : "example@example.com" , "password" : "admin" } # correct password
+        response = app.post("/login", data = data2 , content_type='html/text')
+        #response = app.get("/", content_type='html/text')
+
+        #self.assertEqual(response.status_code, 200)
+        
+        assert b'Admin' in response.data # we hav to work on this because it doesn't take the password in this format
+        
+
