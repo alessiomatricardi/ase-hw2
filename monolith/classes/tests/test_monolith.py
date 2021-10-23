45c4e42af8b290b2a1953b7f11f21e1afee6c65e
Alessio Matricardi <alessiomatricardi@yahoo.com>
2021-10-23 16:00:04 +0200

    simple try

--- monolith/classes/tests/test_monolith.py
+++ monolith/classes/tests/test_monolith.py
@@ -1,16 +1,15 @@
-#from flask.templating import render_template
 from flask.testing import TestCase
+import unittest
 
 from monolith import app as tested_app
 
 #tested_app.testing = True
 
-class TestApp(TestCase):
+class TestApp(unittest.TestCase):
 
     # TODO REMEMBER TO DEFINE SINGLE TESTS METHODS PER EACH FUNCTIONALITY
 
     def test_homepage(self):  
-        self.app = tested_app.test_client()
-        reply = self.app.get("/")
-        self.assert_template_used('index.html')
-        #self.assert_context("greeting", "hello")
+        app = tested_app.test_client()
+        response = app.get("/", content_type='html/text')
+        self.assertEqual(response.status_code, 200)
