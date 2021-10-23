1a769f102cec83d6ed88632d3ef7e278c0da314d
Alessio Matricardi <alessiomatricardi@yahoo.com>
2021-10-23 16:09:42 +0200

    Adjust database for 1st priority user stories (#3)
    * add database stuff
    
    * typo
    
    * fixing error in database - no relationships needed
    
    * unify names
    
    * reintroduce relationship
    
    Co-authored-by: m4r-kus <parti.marco@gmail.com>

--- monolith/database.py
+++ monolith/database.py
@@ -17,11 +17,25 @@ class User(db.Model):
     is_active = db.Column(db.Boolean, default=True)
     is_admin = db.Column(db.Boolean, default=False)
     is_anonymous = False
+    has_picture = db.Column(db.Boolean, default=False)  # has the user a personal profile picture
+
+    '''
+    TODO 2nd priority stuffs
+    lottery_points = db.Column(db.integer, default=0)
+    content_filter_enabled = db.Column(db.Boolean, default=False)
+    '''
+
+    # Relatioships with other classes
+    sent_messages = db.relationship('Message', backref='sender', lazy=True)
+    received_messages = db.relationship('Message_Recipient', backref='recipient', lazy=True)
+    reported_messages = db.relationship('Report', backref='reporter', lazy=True)
+    # blocked_users = db.relationship('Blacklist', backref='blocked', foreign_keys='blocked_user_id', lazy=True)
+    # blocking_users = db.relationship('Blacklist', backref='blocking', foreign_keys='blocking_user_id', lazy=True)
 
     def __init__(self, *args, **kw):
         super(User, self).__init__(*args, **kw)
         self._authenticated = False
-    
+
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
@@ -37,3 +51,90 @@ class User(db.Model):
     def get_id(self):
         return self.id
 
+
+class Message(db.Model):
+
+    __tablename__ = 'message'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.Unicode(700), nullable=False)
+    is_sent = db.Column(db.Boolean, default=False)
+    deliver_time = db.Column(db.DateTime)
+
+    # Relatioship with other classes
+    recipients = db.relationship('Message_Recipient', backref='message', lazy=True)
+    reports = db.relationship('Report', backref='message_report', lazy=True)
+
+    def __init__(self, *args, **kw):
+        super(Message, self).__init__(*args, **kw)
+
+    def set_sender(self, id):
+        self.sender_id = id
+
+    def set_content(self, content):
+        self.content = content
+
+    def get_id(self):
+        return self.id
+
+
+class Message_Recipient(db.Model):
+
+    __tablename__ = 'message_recipient'
+
+    id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    is_read = db.Column(db.Boolean, default=False)
+    read_time = db.Column(db.DateTime)
+
+    __table_args__ = (
+        db.PrimaryKeyConstraint(
+            id, recipient_id,
+        ),
+    )
+
+    def __init__(self, *args, **kw):
+        super(Message_Recipient, self).__init__(*args, **kw)
+
+    def get_id(self):
+        return self.id
+
+    def get_recipient_id(self):
+        return self.recipient_id
+
+
+class Report(db.Model):
+
+    __tablename__ = 'report'
+
+    reporting_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+    report_time = db.Column(db.DateTime)
+
+    __table_args__ = (
+        db.PrimaryKeyConstraint(
+            reporting_user_id, message_id,
+        ),
+    )
+
+
+    def __init__(self, *args, **kw):
+        super(Report, self).__init__(*args, **kw)
+
+
+class Blacklist (db.Model):
+
+    __tablename__ = 'blacklist'
+
+    blocking_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    blocked_user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    __table_args__ = (
+        db.PrimaryKeyConstraint(
+            blocking_user_id, blocked_user_id,
+        ),
+    )
+
+    def __init__(self, *args, **kw):
+        super(Blacklist, self).__init__(*args, **kw)
