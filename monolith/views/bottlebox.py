f5d8f9b0aa6481a2320dda6b9bd0fab3fc392ea9
Alessio Matricardi <alessiomatricardi@yahoo.com>
2021-10-27 13:38:00 +0200

    First message detail implementation

--- monolith/views/bottlebox.py
+++ monolith/views/bottlebox.py
@@ -2,49 +2,75 @@ from flask import Blueprint, redirect, render_template, request
 
 from monolith.database import User, db, Blacklist, Message, Message_Recipient
 from monolith.forms import UserForm
-from monolith.auth import current_user
+from flask_login import current_user
 
 import datetime
 
 
 bottlebox = Blueprint('bottlebox', __name__)
 
-@bottlebox.route('/bottlebox',methods=['GET'])
-def retrieving_bottlebox():
 
+@bottlebox.route('/bottlebox',methods=['GET'])
+def bottlebox_home():
     # checking if there is a logged user
     if current_user is not None and hasattr(current_user, 'id'):
-        v = request.args.get("value")
-        
-        if v == "home":
-            return render_template('bottlebox_home.html')
-        
-        today = datetime.datetime.today()
-        all_users = db.session.query(User).where(User.is_admin == False)
-        msg = []
-
-        # retrieving the pending messages: the ones which have been set to be delivered on a future date
-        if v == "Pending":
-            msg = db.session.query(Message).where(Message.sender_id == current_user.id).where(Message.is_sent == True).where(Message.deliver_time > today)
-
-
-        # retrieving the already received messages
-        elif v == "Received":
-            msg = Message.query.join(Message_Recipient, Message.id == Message_Recipient.id).where(Message_Recipient.recipient_id == current_user.id).where(Message.is_sent == True).where(Message.deliver_time <= today)
-
-        
-        # retrieving the sent and delivered messages: the ones that have been set to be delivered on a past date or on the current date
-        elif v == "Delivered":
-            msg = db.session.query(Message).where(Message.sender_id == current_user.id).where(Message.is_sent == True).where(Message.deliver_time <= today)
-
-
-        return render_template('bottlebox.html', value = v, messages = msg, users = all_users)
-
+        return render_template('bottlebox_home.html')
     else:
         # there is no logged user, redirect to login
         return redirect('/login')
 
 
+@bottlebox.route('/bottlebox/pending', methods=['GET'])
+def show_pending():
+    today = datetime.datetime.today()
+    all_users = db.session.query(User).where(User.is_admin == False)
+    msg = db.session.query(Message).where(Message.sender_id == current_user.id).where(Message.is_sent == True).where(Message.deliver_time > today)
+
+    return render_template('bottlebox.html', messages = msg, users = all_users)
+
+
+@bottlebox.route('/bottlebox/received', methods=['GET'])
+def show_received():
+    today = datetime.datetime.today()
+    all_users = db.session.query(User).where(User.is_admin == False)
+    msg = Message.query.join(Message_Recipient, Message.id == Message_Recipient.id).where(Message_Recipient.recipient_id == current_user.id).where(Message.is_sent == True).where(Message.deliver_time <= today)
+
+    return render_template('bottlebox.html', messages = msg, users = all_users)
+
+
+@bottlebox.route('/bottlebox/delivered', methods=['GET'])
+def show_delivered():
+    today = datetime.datetime.today()
+    all_users = db.session.query(User).where(User.is_admin == False)
+    msg = db.session.query(Message).where(Message.sender_id == current_user.id).where(Message.is_sent == True).where(Message.deliver_time <= today)
+
+    return render_template('bottlebox.html', messages = msg, users = all_users)
+
+
+@bottlebox.route('/message/<id>', methods=['GET'])
+def message_detail(id):
+    # if <id> is not a number, render 404 page
+    try:
+        int(id)
+    except:
+        # TODO 404
+        return render_template('/index.html')
+
+    # checking if there is a logged user
+    if current_user is not None and hasattr(current_user, 'id'):
+        message = db.session.query(Message_Recipient).where(Message_Recipient.id == id).where(Message_Recipient.recipient_id == current_user.id)
+        if message is not None:
+            detailed_message = Message.query.where(Message.id == id).where(Message.is_sent == True)[0]
+            sender = User.query.where(User.id == detailed_message.sender_id)[0]
+            sender_name = sender.firstname + ' ' + sender.lastname
+            return render_template('/message_detail.html', message = detailed_message, sender_name = sender_name)
+        else:
+            # 404 TODO
+            render_template('/index.html')
+    else:
+        # there is no logged user, redirect to login
+        return redirect('/login')
+            
 # @bottlebox.route('/test_message')
 # def create_message():
 #     new_message = Message()
