d46c5db46172c07796760306a59741eab9cba925
RiccardoBonini <riccardobonsi@hotmail.it>
2021-10-29 18:37:15 +0200

    Added further tests on recipients list expected values, total coverage is now 80%
    Co-Authored-By: Marco Parti <89144553+m4r-kus@users.noreply.github.com>

--- monolith/classes/tests/test_recipients_list.py
+++ monolith/classes/tests/test_recipients_list.py
@@ -6,7 +6,7 @@ import datetime
 app.config['WTF_CSRF_ENABLED'] = False
 
 class TestRecipientsList(unittest.TestCase):
-    def test_list_logic(self):
+    def test_list_logic_blacklist(self):
         
         with app.app_context():
             l = ListLogic()
@@ -35,8 +35,35 @@ class TestRecipientsList(unittest.TestCase):
             expected_result = [(2,6), (6,5)]
             self.assertEqual(result, expected_result)
 
-            
+    def test_list_logic_recipients_list(self):
+        
+        with app.app_context():
+            l = ListLogic()
 
+            # 2 should be able to see only 5
+            recipients_list = l.retrieving_recipients(2)
+            result = [b.id for b in recipients_list]
+            expected_result = [5]
+            self.assertEqual(result, expected_result)
 
+            # 3 should be able to see only 6
+            recipients_list = l.retrieving_recipients(3)
+            result = [b.id for b in recipients_list]
+            expected_result = [6]
+            self.assertEqual(result, expected_result)
+
+            # 5 should be able to see only 2
+            recipients_list = l.retrieving_recipients(5)
+            result = [b.id for b in recipients_list]
+            expected_result = [2]
+            self.assertEqual(result, expected_result)
+
+            # 6 should be able to see only 3
+            recipients_list = l.retrieving_recipients(6)
+            result = [b.id for b in recipients_list]
+            expected_result = [3]
+            self.assertEqual(result, expected_result)
+
+          
 
 
