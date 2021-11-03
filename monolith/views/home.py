ef639ee7b95d4fa99cfe62cfb88a40533fea4e57
Marco Parti <89144553+m4r-kus@users.noreply.github.com>
2021-11-03 13:19:29 +0100

    Content Filter (#25)
    Co-authored-by: GManfry <manfredoniagianmarco@gmail.com>

--- monolith/views/home.py
+++ monolith/views/home.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, redirect
-
+from flask import Blueprint, render_template, redirect, request
+from flask.helpers import url_for
 from monolith.auth import current_user
-
+from monolith.content_filter_logic import ContentFilterLogic
+from monolith.database import db, User
 home = Blueprint('home', __name__)
 
 
@@ -14,12 +15,37 @@ def index():
     return render_template("index.html", welcome = welcome)
 
 # show the account information
-@home.route('/account_info')
+@home.route('/account_info', methods=['GET','POST'])
 def account_info():
-    if current_user is not None and hasattr(current_user, 'id'):
-        # show user informations
-        return render_template("account_info.html")
-    else:
-        # TODO
-        # user does not exists
-        return redirect("/login")
+    if request.method == 'GET':
+        if current_user is not None and hasattr(current_user, 'id'):
+            # show user informations
+            return render_template("account_info.html")
+        else:
+            # TODO
+            # user does not exists
+            return redirect("/login")
+
+    elif request.method == 'POST':
+
+        return redirect(url_for('.content_filter'))
+
+@home.route('/account_info/content_filter', methods=['GET'])
+def content_filter():
+    cfl = ContentFilterLogic()
+
+    user = User()
+  
+    user = db.session.query(User).filter(User.id == current_user.id).first()
+    print("this is user object")
+    print(user)
+
+    if user.is_active == True:
+        cfl.de_activate_filter(user.id)
+        msg = "Your content filter has been deactivated"
+
+    elif user.is_active == False:
+        cfl.activate_filter(user.id)
+        msg = "Your content filter has been activated"    
+
+    return render_template('content_filter.html', messages = msg)
