35292bc0bb60617ce38f49e76b6ae5342200680c
RiccardoBonini <riccardobonsi@hotmail.it>
2021-11-05 16:41:15 +0100

    fixing errors in tests activities
    We were creating a message even if there were no recipients
    
    Co-Authored-By: Marco Parti <89144553+m4r-kus@users.noreply.github.com>

--- monolith/views/messages.py
+++ monolith/views/messages.py
@@ -36,7 +36,7 @@ def new_message():
             single_recipient = request.args.get('single_recipient') # used to set a checkbox as checked if the recipient is choosen from the recipient list page
             msg_id = request.args.get('msg_id')
             
-            print(msg_id)
+            print("siamo nel GET, linea 39")
             if msg_id: # if a message id has been given as argument
                 
                 msg = msg_logic.is_my_message(current_user.id, msg_id)
@@ -61,7 +61,7 @@ def new_message():
 
         # the user submits the form to create the new message ("Send bottle" option)
         elif request.method == 'POST': 
-            
+            print("siamo nel POST, linea 63")
             form = request.form
 
             # take message content from form
@@ -71,13 +71,33 @@ def new_message():
             message.deliver_time = datetime.datetime.strptime(form['deliver_time'], '%Y-%m-%dT%H:%M') # !!! DO NOT TOUCH !!!
             id = message.id
 
+            print("siamo a linea 74")
             # validate message content
             if msg_logic.validate_message_fields(message):
                 
+                if len(form.getlist('recipients')) == 0: # if no recipients have been selected
+                    #msg = json.dumps({"msg":"Condition failed on page baz"})
+                    #db.session['msg'] = msg
+                    #return redirect(url_for('.do_foo', messages=messages))
+                    flash("Please select at least 1 recipient")
+                    print("Siamo in recipients < 0, linea 143")
+                    #return redirect(url_for('.new_message'))
+                    return redirect('/new_message') # TODO VEDIAMO COSA SUCCEDE con questo
+
+                print("siamo a linea 78")
+                print(request.form)
                 
+                file_is_attached = False
+                if 'attach_image' in request.form:
+                    print("linea 83")
+                    file_is_attached = True
+                else: 
+                    print("linea 86")
+                    file_is_attached = False
+
                 # add message in the db
-                if not request.files['attach_image'].filename == '': # if the user passes it, save a file in a reposistory and set the field message.image to the filename
-                    
+                if request.files['attach_image'].filename != '': # if the user passes it, save a file in a reposistory and set the field message.image to the filename
+                    print("siamo a linea 91")
                     file = request.files['attach_image']
 
                     if msg_logic.control_file(file): # proper controls on the given file
@@ -93,43 +113,38 @@ def new_message():
                         return redirect('/new_message')
                 
                 else:
+                    print("siamo a linea 107")
                     id = msg_logic.create_new_message(message)['id']
 
             else:
+                print("siamo a linea 111")
                 # TODO handle incorrect message fields
-                return render_template('/error_page.html')
+                return render_template('error.html')
             
-            if len(form.getlist('recipients')) > 0: # if no recipients have been selected
-                for recipient_email in form.getlist('recipients'): # list of selected recipients emails
-                    message_recipient = Message_Recipient()
+           
+            for recipient_email in form.getlist('recipients'): # list of selected recipients emails
+                message_recipient = Message_Recipient()
 
-                    recipient_id = msg_logic.email_to_id(recipient_email)
+                recipient_id = msg_logic.email_to_id(recipient_email)
 
-                    # initialize the Message_Recipient object
-                    message_recipient.id = id
-                    print(id)
-                    message_recipient.is_read = False # redundant because the db automatically set it to False
-                    message_recipient.recipient_id = recipient_id
-                    #msg_logic.create_new_message_recipient(message_recipient)
+                # initialize the Message_Recipient object
+                message_recipient.id = id
+                print('Siamo in recipients > 0, linea 123')
+                message_recipient.is_read = False # redundant because the db automatically set it to False
+                message_recipient.recipient_id = recipient_id
+                msg_logic.create_new_message_recipient(message_recipient)
 
-                if form['submit'] == 'Send bottle': # if it is a draft, the is_sent flag will not be set to True
-                    msg_logic.send_bottle(message) 
-                
-                """elif form['submit'] == 'Save draft':
-                    pass"""
-                    # seconds = (message.deliver_time - datetime.datetime.now()).total_seconds() 
-                    # msg_logic.send_notification.apply_async(countdown=seconds, kwargs={'sender_email': current_user.email, 'recipients_list': form.getlist('recipients')})
-                
-                return render_template("index.html") 
+            if form['submit'] == 'Send bottle': # if it is a draft, the is_sent flag will not be set to True
+                msg_logic.send_bottle(message) 
+            
+            """elif form['submit'] == 'Save draft':
+                pass"""
+                # seconds = (message.deliver_time - datetime.datetime.now()).total_seconds() 
+                # msg_logic.send_notification.apply_async(countdown=seconds, kwargs={'sender_email': current_user.email, 'recipients_list': form.getlist('recipients')})
+            
+            return render_template("index.html") 
 
-            else:
-                #msg = json.dumps({"msg":"Condition failed on page baz"})
-                #db.session['msg'] = msg
-                #return redirect(url_for('.do_foo', messages=messages))
-                flash("Please select at least 1 recipient")
-                #return redirect(url_for('.new_message'))
-                return redirect('/new_message') # TODO VEDIAMO COSA SUCCEDE con questo
-
+        
             """
             TEST
             import unittest
