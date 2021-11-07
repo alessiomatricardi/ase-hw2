0226a609702239775dbb518f2c1dc3580bd6bcec
Alessio Matricardi <alessiomatricardi@yahoo.com>
2021-11-07 23:26:27 +0100

    Attachments folder renaming (#37)

--- test.sh
+++ test.sh
@@ -1,14 +1,15 @@
 echo 'Actual db will be renamed to old_mmiab.db'
 echo 'Remember to rename it if you want to use'
 mv -f mmiab.db old_mmiab.db || echo 'mmiab.db not exists... continue with tests'
-echo 'monolith/static/attached folder has been moved into root directory'
-mv -f monolith/static/attached ./old_attached
-mkdir monolith/static/attached
+echo 'monolith/static/attachments folder has been moved into root directory'
+mv -f monolith/static/attachments ./old_attachments
+mkdir monolith/static/attachments
 pytest -s
 if [[ $1 -eq 1 ]]
 then
     rm -rf mmiab.db
     mv -f old_mmiab.db mmiab.db || echo 'old_mmiab.db not exists... impossible to restore it'
-    rm -rf monolith/static/attached
-    mv -f ./old_attached monolith/static/attached
+    rm -rf monolith/static/attachments
+    mv -f ./old_attachments monolith/static/attachments
 fi
+echo 'Test done!'
