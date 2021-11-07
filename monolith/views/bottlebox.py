883d831009754fbebe357104d69b791b6c10bf5c
Marco Parti <89144553+m4r-kus@users.noreply.github.com>
2021-11-07 12:06:04 +0100

    Moved logic features to logic classes (#33)
    in handling drafts, pending, delivered and received messages
    
    Co-authored-by: RiccardoBonini <RiccardoBonini@users.noreply.github.com>
    
    Co-authored-by: RiccardoBonini <RiccardoBonini@users.noreply.github.com>

--- monolith/views/bottlebox.py
+++ monolith/views/bottlebox.py
@@ -1,3 +1,4 @@
+from re import I
 from celery.utils.functional import first
 from flask import Blueprint, redirect, render_template, request, abort
 from flask.globals import current_app
@@ -7,7 +8,7 @@ from sqlalchemy.sql.expression import false
 from monolith.database import User, db, Blacklist, Message, Message_Recipient
 from monolith.forms import UserForm, MessageForm
 from flask_login import current_user
-from monolith.bottlebox_logic import BottleBoxLogic
+from monolith.bottlebox_logic import BottleBoxLogic, DraftLogic
 from sqlalchemy.sql import or_,and_
 from monolith.emails import send_email
 from monolith.message_logic import MessageLogic
@@ -16,6 +17,7 @@ from werkzeug.utils import secure_filename
 
 import datetime
 import os
+import shutil
 
 
 bottlebox = Blueprint('bottlebox', __name__)
@@ -102,11 +104,12 @@ def delivered_detail(label, id):
         detailed_message = None
         blocked = None
 
+        bottlebox_logic = BottleBoxLogic()
+
         # checking the value of the label
         if label == 'received':
 
-            detailed_message = Message.query.where(Message.id == id).where(Message.is_sent == True)
-            detailed_message = [ob for ob in detailed_message]
+            detailed_message = bottlebox_logic.retrieve_received_message(id)
 
             # checking that the <id> message exists
             if not detailed_message:
@@ -115,29 +118,21 @@ def delivered_detail(label, id):
             detailed_message = detailed_message[0]
 
             # checking if the current_user is into recipients of the message
-            message_recipient = db.session.query(Message_Recipient).where(and_(Message_Recipient.id == id,Message_Recipient.recipient_id == current_user.id))
-            message_recipient = [ob for ob in message_recipient]
+            message_recipient = bottlebox_logic.is_recipient(id,current_user.id)
+            
             if not message_recipient:
                 abort(404)
 
             # check if is_read == False. If so, set it to True and send notification to sender
             if message_recipient[0].is_read == False:
-                db.session.query(Message_Recipient).where(and_(Message_Recipient.id == id,Message_Recipient.recipient_id == current_user.id)).update({'is_read': True})
-                db.session.commit()
-                msg = "Subject: Message notification\n\nThe message you sent to " + current_user.firstname + " has been read."
-                message_sender_id = db.session.query(Message).filter(Message.id == id).first().sender_id
-                email = db.session.query(User).filter(User.id == message_sender_id).first().email
-                send_email(email, msg)
-
+                
+                if not bottlebox_logic.notify_on_read(id,current_user):
+                    abort(404)
 
             other_id = detailed_message.sender_id
 
-            # checking if the message is from a blocked or blocking user 
-            blacklist_istance = db.session.query(Blacklist).where(or_(
-                                                                        and_(Blacklist.blocked_user_id == current_user.id, Blacklist.blocking_user_id == other_id),
-                                                                        and_(Blacklist.blocked_user_id == other_id, Blacklist.blocking_user_id == current_user.id)
-                                                                    ))
-            blacklist_istance = [ob for ob in blacklist_istance]
+            # checking if the message is from a blocked or blocking user
+            blacklist_istance = bottlebox_logic.user_blacklist_status(other_id,current_user.id)
             
             # blocked variable is passed to render_template in order to display or not the reply and block buttons
             if not blacklist_istance:
@@ -149,6 +144,7 @@ def delivered_detail(label, id):
         elif label == 'draft': 
             
             msg_logic = MessageLogic()
+            draft_logic = DraftLogic()
 
             # rendering the draft detail
             if request.method == 'GET':
@@ -156,38 +152,30 @@ def delivered_detail(label, id):
                 form.recipients.choices = msg_logic.get_list_of_recipients_email(current_user.id) 
 
                 # retrieving the message, if exists
-                detailed_message = Message.query.where(Message.id == id).where(Message.is_sent == False)
-                detailed_message = [ob for ob in detailed_message]
+                detailed_message = draft_logic.retrieve_draft(id)
                 if not detailed_message:
                     abort(404)
 
                 detailed_message = detailed_message[0]
 
                 recipients = []
-                recipients_id = []
+                # recipients_id = []
                 recipients_emails = []
 
                 # checking if the current user is the sender, then retrieving recipients of draft
                 if detailed_message.sender_id == current_user.id:
-                    recipients_id = db.session.query(Message_Recipient).where(Message_Recipient.id == id)
-                    recipients_id = [ob.recipient_id for ob in recipients_id]
-                    recipients = User.query.filter(User.id.in_(recipients_id))
-                    recipients = [ob for ob in recipients]
+                    recipients = draft_logic.retrieve_current_draft_recipients(id)
                 else:
                     abort(404)
 
                 # checking that already saved recipients are still available (they could have become inactive or blocked/blocking user)
                 for recipient in recipients:
-                    blacklist_istance = db.session.query(Blacklist).where(or_(
-                                                                    and_(Blacklist.blocked_user_id == current_user.id, Blacklist.blocking_user_id == recipient.id),
-                                                                    and_(Blacklist.blocked_user_id == recipient.id, Blacklist.blocking_user_id == current_user.id)
-                                                                ))
-                    blacklist_istance = [ob for ob in blacklist_istance]
+                    blacklist_istance = draft_logic.recipient_blacklist_status(current_user.id,recipient.id)
                     if len(blacklist_istance) > 0 or not recipient.is_active:
                         # the user is no longer available to receive messages from current_user either being inactive or being blocked/blocking
                         flash("The user " + str(recipient.email) + " is no longer avaiable")
-                        db.session.query(Message_Recipient).where(and_(Message_Recipient.id == detailed_message.id,Message_Recipient.recipient_id == recipient.id)).delete()
-                        db.session.commit()
+                        if not draft_logic.remove_unavailable_recipient(detailed_message.id,recipient.id):
+                            abort(404)
                     else:
                         # the saved recipient is still available
                         recipients_emails.append(recipient.email)
@@ -203,8 +191,7 @@ def delivered_detail(label, id):
                 form = request.form
 
                 # retrieving the draft to send, modifiy or delete it
-                detailed_message = Message.query.where(Message.id == id).where(Message.is_sent == False)
-                detailed_message = [ob for ob in detailed_message]
+                detailed_message = draft_logic.retrieve_draft(id)
                 # checks that the draft exists
                 if not detailed_message:
                     abort(404)
@@ -222,24 +209,8 @@ def delivered_detail(label, id):
                 # delete draft from db, eventual image in filesystem and all message_recipients instances 
                 if form['submit'] == 'Delete draft': 
 
-                    # deleting instances of recipients
-                    db.session.query(Message_Recipient).where(Message_Recipient.id == detailed_message.id).delete()
-
-                    # deleting previously attached image, if it exists
-                    if detailed_message.image != '':
-                        
-                        # directory to the folder in which is stored the image
-                        directory = os.path.join(os.getcwd(), 'monolith', 'static', 'attached', str(detailed_message.id))
-                        myfile = os.path.join(directory, detailed_message.image)
-
-                        # deleting image and directory
-                        if os.path.isfile(myfile):
-                            os.remove(myfile)
-                            os.rmdir(directory)
-
-                    # deleting the draft from database and committing all changes
-                    db.session.query(Message).where(Message.id == detailed_message.id).delete()
-                    db.session.commit()
+                    if not draft_logic.delete_draft(detailed_message):
+                        abort(404)
 
                     return render_template("index.html")
 
@@ -251,39 +222,25 @@ def delivered_detail(label, id):
                     # retrieving id of recipient
                     recipient_id = msg_logic.email_to_id(recipient_email)
 
-                    # retrieving eventual instance of message_recipient for current draft and recipient
-                    message_recipient = db.session.query(Message_Recipient).where(Message_Recipient.id == detailed_message.id).where(Message_Recipient.recipient_id == recipient_id).all()
-                    
-                    # checks if there is not instance 
-                    if len(message_recipient) == 0:
-                        
-                        # creates instance of new message_recipient in order to possibly send the message to the new selected recipient
-                        message_recipient = Message_Recipient()
-                        message_recipient.id = detailed_message.id
-                        message_recipient.recipient_id = recipient_id
-
-                        # adds row to db
-                        db.session.add(message_recipient)
-                        db.session.commit()
+                    # add recipient to draft if not already stored
+                    if not draft_logic.update_recipients(detailed_message,recipient_id):
+                        abort(404)
 
                 # update content of message: if the content is not changed, it'll store the same value
-                db.session.query(Message).where(Message.id == detailed_message.id).update({"content": form['content']})
+                if not draft_logic.update_content(detailed_message,form):
+                    abort(404)
                 # update the deliver time for the draft
-                db.session.query(Message).where(Message.id == detailed_message.id).update({"deliver_time": datetime.datetime.strptime(form['deliver_time'], '%Y-%m-%dT%H:%M')})
+                if not draft_logic.update_deliver_time(detailed_message,form):
+                    abort(404)
                 
                 # checking if there is a new attached image in the form 
                 if request.files and request.files['attach_image'].filename != '':
-                    
+
                     # checking if there's a previous attached image, if so we delete it
                     if detailed_message.image != '':
-                        
-                        # directory where is stored the attached imaged
-                        directory = os.path.join(os.getcwd(), 'monolith', 'static', 'attached', str(detailed_message.id))
-                        myfile = os.path.join(directory, detailed_message.image)
-                       
-                        # deleting the previously attached image
-                        if os.path.isfile(myfile):
-                            os.remove(myfile)
+
+                        if not draft_logic.delete_previously_attached_image(detailed_message):
+                            abort(404)
                     
                     # retrieving newly attached image
                     file = request.files['attach_image']
@@ -291,25 +248,8 @@ def delivered_detail(label, id):
                     # proper controls on the given file
                     if msg_logic.control_file(file): 
 
-                        # updating the name of attached image inside db
-                        db.session.query(Message).where(Message.id == detailed_message.id).update({"image": secure_filename(file.filename)})
-                        
-                        id = detailed_message.id
-
-                        # TODO trycatch
-                        attached_dir = os.path.join(os.getcwd(),'monolith','static','attached')
-                        
-                        # creating attached folder, if it doesn't already exist
-                        if not os.path.exists(attached_dir):
-                            os.makedirs(attached_dir)
-
-                        # creating attached image folder, if it doesn't already exist
-                        if not os.path.exists(os.path.join(attached_dir, str(id))):
-                            # TODO try catch
-                            os.mkdir(os.path.join(os.getcwd(),'monolith','static','attached',str(id)))
-
-                        # saving the attached image
-                        file.save(os.path.join(os.getcwd(),'monolith','static','attached',str(id),secure_filename(file.filename)))
+                        if not draft_logic.update_attached_image(detailed_message,file):
+                            abort(404)
 
                     else:
                         # control on filename fails
@@ -319,9 +259,8 @@ def delivered_detail(label, id):
                 # the draft is sent and its is_sent attribute is set to 1, from now on it's no longer possible to modify it
                 # in order to stop it, it'll be necessary to spend lottery points
                 if form['submit'] == 'Send bottle':
-                    db.session.query(Message).where(Message.id == detailed_message.id).update({"is_sent": 1})
-                
-                db.session.commit()
+                    if not draft_logic.send_draft(detailed_message):
+                        abort(404)
 
                 return render_template("index.html")
 
@@ -329,8 +268,11 @@ def delivered_detail(label, id):
         else: # case label is pending or delivered
 
             # checks that message exists
-            detailed_message = Message.query.where(Message.id == id).where(Message.is_sent == True)
-            detailed_message = [ob for ob in detailed_message]
+            if label == 'pending':
+                detailed_message = bottlebox_logic.retrieve_pending_message(id)
+            elif label == 'delivered':
+                detailed_message = bottlebox_logic.retrieve_delivered_message(id)
+        
             if not detailed_message:
                 abort(404)
 
@@ -338,23 +280,18 @@ def delivered_detail(label, id):
 
             # checking if the current user is the sender
             if detailed_message.sender_id == current_user.id:
-                recipients_id = db.session.query(Message_Recipient).where(Message_Recipient.id == id)
-                recipients_id = [ob.recipient_id for ob in recipients_id]
-                recipients = User.query.filter(User.id.in_(recipients_id))
-                recipients = [ob for ob in recipients]
+                recipients = bottlebox_logic.retrieve_recipients(id)
             else:
                 abort(404)
             
             other_id = None
+
             # checks if a recipient has blocked the current_user or has been blocked
             for i in range(len(recipients)):
                 other_id = recipients[i].id
-                blacklist_istance = db.session.query(Blacklist).where(or_(
-                                                                    and_(Blacklist.blocked_user_id == current_user.id, Blacklist.blocking_user_id == other_id),
-                                                                    and_(Blacklist.blocked_user_id == other_id, Blacklist.blocking_user_id == current_user.id)
-                                                                ))
-                blacklist_istance = [ob for ob in blacklist_istance]
                 
+                blacklist_istance = bottlebox_logic.user_blacklist_status(other_id,current_user.id)
+
                 # appends to blocked_info a tuple to link the respective recipient and its blacklist status 
                 if not blacklist_istance:
                     blocked_info.append([recipients[i], False])
@@ -364,6 +301,7 @@ def delivered_detail(label, id):
         # retrieving sender info from db
         sender = User.query.where(User.id == detailed_message.sender_id)[0]
         sender_name = sender.firstname + ' ' + sender.lastname 
+        sender_name = bottlebox_logic.retrieve_sender_info(detailed_message)
 
         return render_template('/message_detail.html', message = detailed_message, sender_name = sender_name, sender_email = sender.email, blocked = blocked, recipients = blocked_info, label = label)
 
